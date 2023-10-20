a32e476ed92dfa7b9091c0789a871e548036af25
jenab <jenab@BIKRAM>
2023-10-20 17:28:21 +0530

    commit1

---
+++ com.GithubPractice/src/main/java/com/GithubPractice/GitHubPractice.java
@@ -0,0 +1,13 @@
+package com.GithubPractice;
+
+public class GitHubPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("GithubPractrice");
+		
+		
+	}
+
+}
